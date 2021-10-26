35b9b7763f173a2705a4eaf2141488695e6e301b
Ahmed Mouzoune <ahmedmouzoune@MacBook-Pro-de-Ahmed.local>
2021-10-26 17:23:52 +0200

    Ajout des commentaires sur indexphp

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
+// Import de la classe utilisateur avec require pour faire crash l'app si ça ne l'importe pas
 require './class/Utilisateur.class.php';
-
+// Initialisation du tableau pour l'avoir même s'il est vide
 $users = [];
+// Créations des différents users
 $user1 = new Utilisateur('Donald', 'Trump');
 $user2 = new Utilisateur('Barack', 'Obama');
 $user3 = new Utilisateur('Hilary', 'Clinton');
-
+// Ajout des différents users au tableau
 $users[] = $user1;
 $users[] = $user2;
 $users[] = $user3;
@@ -21,6 +23,7 @@ $users[] = $user3;
 <body>
     <ul>
         <?php
+            // Boucle qui parcourt le tableau users et affiche un li avec la fonction getName pour afficher tout les noms prénoms des users en liste
             foreach($users as $user) {
                 echo('<li>'.$user->getName().'</li>');
             }
